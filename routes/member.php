0391bfeb36610e5ba8097375e70e101c7333bca6
RobbyIzhar <aramadhana70@gmail.com>
2022-05-28 10:34:43 +0700

    change-member-routes-auth

--- routes/member.php
+++ routes/member.php
@@ -1,14 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController AS Login;
-use App\Http\Controllers\Auth\LoginController;
+// use App\Http\Controllers\LoginController AS Login;
+// use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Member\HomeController;
 
-Route::get('/', [Login::class, 'index']);
-Route::post('login-qr', [LoginController::class, 'loginQR'])->name('login-qr');
+// Route::get('/', [Login::class, 'index']);
+// Route::post('login-qr', [LoginController::class, 'loginQR'])->name('login-qr');
 
-Auth::routes();
+// Auth::routes();
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('member');
